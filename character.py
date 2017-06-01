846d60b6ab8972cbcebeb6f4dd811a7033fe005d
Laura <laura.sach@raspberrypi.org>
2017-06-01 15:24:24 +0100

    Add character class

---
+++ character.py
@@ -0,0 +1,28 @@
+class Character():
+
+    # Create a character
+    def __init__(self, char_name, char_description):
+        self.name = char_name
+        self.description = char_description
+        self.conversation = None
+
+    # Describe this character
+    def describe(self):
+        print( self.name + " is here!" )
+        print( self.description )
+
+    # Set what this character will say when talked to
+    def set_conversation(self, conversation):
+        self.conversation = conversation
+
+    # Talk to this character
+    def talk(self):
+        if self.conversation is not None:
+            print("[" + self.name + " says]: " + self.conversation)
+        else:
+            print(self.name + " doesn't want to talk to you")
+
+    # Fight with this character
+    def fight(self, combat_item):
+        print(self.name + " doesn't want to fight with you")
+        return True
