3d80545f245e1f3a43f0cf9b3c9c7b4a9fd98b79
Laura <laura.sach@raspberrypi.org>
2018-11-28 15:28:09 +0000

    Update name so it isn't confusing

--- challenges/turtle_race.py
+++ challenges/turtle_race.py
@@ -1,16 +1,16 @@
 from turtle import Turtle
 from random import randint
 
-paul = Turtle()
-paul.color('red')
-paul.shape('turtle')
-paul.penup()
-paul.goto(-160, 100)
-paul.pendown()
+john = Turtle()
+john.color('red')
+john.shape('turtle')
+john.penup()
+john.goto(-160, 100)
+john.pendown()
 
 # Create three more instances of Turtle and make them race!
 # Make sure each turtle starts slightly below the last so that they
 # are not racing on top of each other
 
 for movement in range(100):
-    paul.forward(randint(1,5))
+    john.forward(randint(1,5))
